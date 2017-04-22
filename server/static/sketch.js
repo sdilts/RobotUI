89b04497ffbe2789917e05e902d4c95bf99b69b9
JordanPottruff <JordanPottruff@gmail.com>
2017-04-22 17:24:59 -0600

    Added edge creation to user interface.

--- server/static/sketch.js
+++ server/static/sketch.js
@@ -2,7 +2,8 @@ var cnv;  //Canvas object.
 var bckColor; //Backgorund color.
 var digraph;  //Directed graph data structure.
 var takenNames = []; //Array of taken vertex names for checking.
-var selected; //Boolean for whether a vertex is selected.
+var selected = null; //Currently selected vertex for edge creation.
+var selLine = []; //Array of two points for drawing a selection line.
 
 var nameLabel; //Label for name input.
 var nameInput; //Name input.
@@ -32,10 +33,10 @@ function test(){
   digraph.addVertex(v3);
   digraph.addVertex(v4);
   digraph.addVertex(v5);
-  digraph.addEdge(v1, v2);
-  digraph.addEdge(v3, v2);
-  digraph.addEdge(v5, v3);
-  digraph.addEdge(v3, v5);
+  digraph.addEdge(v1.name, v2.name);
+  digraph.addEdge(v3.name, v2.name);
+  digraph.addEdge(v5.name, v3.name);
+  digraph.addEdge(v3.name, v5.name);
   console.log(JSON.stringify(digraph.matrix));
   console.log(JSON.stringify(digraph.vertices));
 }
@@ -46,6 +47,9 @@ function test(){
 function draw(){
   background(54, 63, 69);
   digraph.draw();
+  stroke(255);
+  strokeWeight(2);
+  //line(selLine[0], selLine[1], selLine[2], selLine[3]);
 }
 
 function setInterface(){
@@ -57,9 +61,45 @@ function setInterface(){
 
 function clearGraph(){
   digraph = new Digraph();
+  takenNames = [];
 }
 
 function mousePressed(){
+  if(selected === null){
+    let collision = checkClick(mouseX, mouseY);
+    if(collision !== null){
+      selected = collision;
+    }else{
+      createVertex();
+    }
+  }else{
+    let collision = checkClick(mouseX, mouseY);
+    if(collision === null){
+      selected = null;
+    }else{
+      digraph.addEdge(selected, collision);
+      selected = null;
+    }
+  }
+}
+
+function mouseMoved(){
+
+}
+
+/*
+ *  Check if the mouse has clicked a vertex.
+ */
+function checkClick(x, y){
+  for(let key in digraph.vertices){
+    if(dist(digraph.vertices[key]['x'], digraph.vertices[key]['y'], x, y) < 15){
+      return key;
+    }
+  }
+  return null;
+}
+
+function createVertex(){
   //Is the name taken?
   let taken = takenNames.indexOf(nameInput.value()) != -1;
   //Is the name empty?
